48f5c9d154794724f2d5b19740b7fa88db774bf7
Adit <adityahimaone@gmail.com>
2021-11-01 18:51:16 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './parts/Navbar'
+import Header from './parts/Header'
+import Portofolio from './parts/SectionPortofolio'
+import About from './parts/SectionAbout'
+import Contact from './parts/Contact'
+import Footer from './parts/Footer'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      <Header></Header>
+      <Portofolio></Portofolio>
+      <About></About>
+      {/* <Contact></Contact> */}
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/assets/js/script.js
@@ -0,0 +1,62 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+
+////////////////////////////////
+
+function saveContact() {
+  if (typeof Storage !== "undefined") {
+    // Store
+    let setContact = [];
+    setContact[0] = document.getElementById("fullname-input").value;
+    localStorage.setItem("contact", JSON.stringify(setContact));
+
+    setContact[1] = document.getElementById("email-input").value;
+    localStorage.setItem("contact", JSON.stringify(setContact));
+
+    setContact[2] = document.getElementById("phonenumber-input").value;
+    localStorage.setItem("contact", JSON.stringify(setContact));
+
+    setContact[3] = document.getElementById("national-input").value;
+    localStorage.setItem("contact", JSON.stringify(setContact));
+
+    setContact[4] = document.getElementById("message-input").value;
+    localStorage.setItem("contact", JSON.stringify(setContact));
+
+    // consle.log(document.getElementById("fullname-review").innerHTML = JSON.parse(localStorage.getItem("contact")))
+    // document.getElementById("email-review").innerHTML = JSON.parse(localStorage.getItem("contact"));
+    // document.getElementById("phone-review").innerHTML = JSON.parse(localStorage.getItem("contact"));
+    // document.getElementById("national-review").innerHTML = JSON.parse(localStorage.getItem("contact"));
+    // document.getElementById("msg-review").innerHTML = JSON.parse(localStorage.getItem("contact"));
+  }
+}
+
+function getContact() {
+  // Retrieve
+  let getContact = JSON.parse(localStorage.getItem("contact"));
+  document.getElementById("fullname-review").innerHTML = getContact[0];
+  document.getElementById("email-review").innerHTML = getContact[1];
+  document.getElementById("phone-review").innerHTML = getContact[2];
+  document.getElementById("national-review").innerHTML = getContact[3];
+  document.getElementById("msg-review").innerHTML = getContact[4];
+}
